a204424bf01ea0f95397e144fc39caf37c59f8c1
linhphung211 <lphungtuan2004@gmail.com>
2026-04-03 01:20:40 +0700

    [3/4/2026 1:20 AM]: Config beck-end

---
+++ Back-end/turtle_project/apps/user/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import User  # Import model của bạn
+
+admin.site.register(User)
+
--- Back-end/turtle_project/core/urls.py
+++ Back-end/turtle_project/core/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for back_end_turtle project.
+URL configuration for core project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
@@ -20,3 +20,7 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+admin.site.site_header = "Turtle Web Administration"
+admin.site.site_title = "Turtle Admin Portal"
+admin.site.index_title = "Chào mừng đến với hệ thống quản trị Turtle"
